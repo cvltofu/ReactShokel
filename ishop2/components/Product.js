205b61ad62e46bcabd0d4e11f80202d4835c6bae
dmphaze <kexit353@gmail.com>
2025-11-10 22:20:22 +0300

    remake selection with guide and add delete

--- ishop2/components/Product.js
+++ ishop2/components/Product.js
@@ -3,32 +3,31 @@ import React from 'react'
 import './Product.css'
 
 class Product extends React.Component {
-	state = {
-		productClassName: 'ShopElement',
-	}
-
-	setSelected = () => {
-		this.state.productClassName.includes(' selected')
-			? this.setState({
-					productClassName: this.state.productClassName.replace(
-						' selected',
-						''
-					),
-			  })
-			: this.setState({
-					productClassName: (this.state.productClassName += ' selected'),
-			  })
+	delete = eo => {
+		//eo.stopPropagation()
+		this.props.cbDeleteItem(this.props.code)
 	}
 
 	render() {
 		return (
-			<li className={this.state.productClassName} onClick={this.setSelected}>
+			<li
+				className='ShopElement'
+				onClick={() => this.props.cbSelectItem(this.props.code)}
+				style={{
+					backgroundColor: this.props.isSelected
+						? '#376aacff'
+						: 'rgb(187, 212, 235)',
+				}}
+			>
 				<span className='Name'>{this.props.name}</span>
 				<span className='Price'>{this.props.price}</span>
 				<span className='Count'>{this.props.count}</span>
 				<span className='Photo'>
 					<img src={this.props.photoURL} alt=''></img>
 				</span>
+				<button className='deleteButton' onClick={() => this.delete()}>
+					<img className='deleteButtonImg' src='./images/bin.png'></img>
+				</button>
 			</li>
 		)
 	}
--- ishop2/components/Shop.js
+++ ishop2/components/Shop.js
@@ -5,14 +5,33 @@ import './Shop.css'
 import Product from './Product'
 
 class Shop extends React.Component {
+	state = {
+		selectedItemCode: null,
+		shopItems: this.props.products,
+	}
+
+	selectItem = code => {
+		this.setState({ selectedItemCode: code })
+	}
+
+	deleteItem = code => {
+		this.setState({
+			shopItems: this.state.shopItems.filter(item => item.id !== code),
+		})
+	}
+
 	render() {
-		const shopItems = this.props.products.map(item => (
+		const shopItems = this.state.shopItems.map(item => (
 			<Product
 				key={item.id}
+				code={item.id}
 				name={item.name}
 				price={item.price}
 				photoURL={item.photoURL}
 				count={item.count}
+				isSelected={item.id === this.state.selectedItemCode}
+				cbSelectItem={this.selectItem}
+				cbDeleteItem={this.deleteItem}
 			/>
 		))
 
