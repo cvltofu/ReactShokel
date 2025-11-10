279ff6a66541f617b838199c7e56772e5e69d9aa
dmphaze <kexit353@gmail.com>
2025-11-10 20:23:21 +0300

    add item selection

--- ishop2/components/Product.js
+++ ishop2/components/Product.js
@@ -3,9 +3,26 @@ import React from 'react'
 import './Product.css'
 
 class Product extends React.Component {
+	state = {
+		productClassName: 'ShopElement',
+	}
+
+	setSelected = () => {
+		this.state.productClassName.includes(' selected')
+			? this.setState({
+					productClassName: this.state.productClassName.replace(
+						' selected',
+						''
+					),
+			  })
+			: this.setState({
+					productClassName: (this.state.productClassName += ' selected'),
+			  })
+	}
+
 	render() {
 		return (
-			<li className='ShopProduct'>
+			<li className={this.state.productClassName} onClick={this.setSelected}>
 				<span className='Name'>{this.props.name}</span>
 				<span className='Price'>{this.props.price}</span>
 				<span className='Count'>{this.props.count}</span>
